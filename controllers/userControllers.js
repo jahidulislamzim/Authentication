f30f9faaa529e25ee6f192230cc17b8bf4f3230f
jahidulislamzim <jahidulislamzim845@gmail.com>
2022-08-31 23:29:44 +0600

    adding lax

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -358,10 +358,10 @@ const resetPassword = async (req, res) => {
 }
 
 const testCookies = async (req, res) =>{
-    res.cookie('None', 'External', { sameSite: 'none', secure: true })
-    // res.cookie('Lax', 'External', { sameSite: 'lax', secure: true})
+    // res.cookie('None', 'External', { sameSite: 'none', secure: true })
+    res.cookie('Lax', 'External', { sameSite: 'lax', secure: true})
     // res.cookie('Strict', 'External', { sameSite: 'strict', secure: true })
-    res.send('External cookies set.')
+    res.send({ "status": "failed", "message": "Invalid Link" })
 }
 
 

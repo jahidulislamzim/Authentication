dc3f0210d5529f7df3c627c25a0b43c3b4f1015d
jahidulislamzim <jahidulislamzim845@gmail.com>
2022-08-31 23:02:24 +0600

    adding bla

--- index.js
+++ index.js
@@ -18,13 +18,12 @@ app.use(
   })
 );
 
-
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   next()
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 
 
 // app.use((req, res, next) => {
